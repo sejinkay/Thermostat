b15136a60f7eeec3f13e853e69ee09756bddb0cc
sejinkay <sejinkay@gmail.com>
2019-04-29 18:29:58 +0100

    installed jasmine, created fizzbuzz test(not passing yet)

---
+++ jasmine/src/fizzBuzz.js
@@ -0,0 +1,15 @@
+function FizzBuzz() {
+}
+  FizzBuzz.prototype.play = function(number) {
+    var fizzBuzz = [1..number];
+    var fizzfizz = fizzBuzz.forEach(function(num) {
+    return(fizzfizz)
+  })};
+
+
+  // passing first test
+  // function FizzBuzz() {
+  // }
+  //   FizzBuzz.prototype.play = function(number) {
+  //     return "1"
+  //   };
